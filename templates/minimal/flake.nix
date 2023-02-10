6df6abc6712bd4048147c7d1dd161e989df61bbb
Takuma Yoneda <takuma-yoneda@users.noreply.github.com>
2023-02-09 18:29:23 -0600

    update mk-darwin-system.url

--- templates/minimal/flake.nix
+++ templates/minimal/flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     # change main to a tag o git revision
-    mk-darwin-system.url = "github:vic/mk-darwin-system/main";
+    mk-darwin-system.url = "github:takuma-yoneda/mk-darwin-system/main";
     mk-darwin-system.inputs.nixpkgs.follows = "nixpkgs";
   };
 
